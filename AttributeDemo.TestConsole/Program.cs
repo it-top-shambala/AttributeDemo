b6b64d53b99dd17839b9a9b9253e65f2b171bb3c
Starinin Andrey <starininandrey@gmail.com>
2022-12-15 17:28:01 +0300

    Add TestConsole

---
+++ AttributeDemo.TestConsole/Program.cs
@@ -0,0 +1,9 @@
+﻿using AttributeDemo.AttributeLib;
+using AttributeDemo.TestConsole;
+
+var test = new TestClass { Str = "S", Digit = 5, List = new List<string> { "first", "second", string.Empty } };
+
+await UniversalSerializer.SerializeAsync(test);
+
+await test.SerializeToJsonAsync("test_2.json");
+await test.SerializeToXmlAsync("test_2.xml");
---
+++ AttributeDemo.TestConsole/TestClass.cs
@@ -0,0 +1,12 @@
+﻿using AttributeDemo.AttributeLib.Attributes;
+
+namespace AttributeDemo.TestConsole;
+
+[Json("test_class")]
+[Xml("test_class")]
+public class TestClass
+{
+    public string Str { get; set; }
+    public int Digit { get; set; }
+    public List<string> List { get; set; }
+}
